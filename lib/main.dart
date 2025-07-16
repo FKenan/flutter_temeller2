c1bb794b19a3b8818abd502f7187d4ef2ad5be42
FKenan <kenanf734@gmail.com>
2025-07-16 22:08:03 +0300

    Uygulama genelinde Wİdget özelinde tema atama

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        // uygulama genelinde widgetlara özel tema atamak için burda belirtiriz.
         useMaterial3: false,
+        textTheme: TextTheme(
+          labelLarge: TextStyle(
+            fontSize: 24,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ), // uygulamanın genelinde verilen text teması
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          // uygulamanın floating buttonlarının kullanacağı default tema
+          backgroundColor: Colors.yellow,
+        ),
         primarySwatch: Colors
             .pink, // primarySwatch ile tema renki verdik butonlara appbara vb burda belirlediğimiz renk verilir.
       ),
@@ -36,6 +47,9 @@ class _HomePageState extends State<HomePage> {
 
   void _sayacArttir() {
     // statefullWidget classından extend edince setState kullanabiliriz.
+    // setState ile state i güncelliyoruz. Reacttan farklı olarak uygulamanın güncellenmesi gereken yerlerde setState kullanırız.
+    // setState çağırılınca ilgili widgetin buildini tekrar çağırır.
+    // kodları setState içine yazmamız gerekmez.
     setState(() {
       _sayac++;
     });
@@ -57,9 +71,17 @@ class _HomePageState extends State<HomePage> {
           children: [
             Text(
               "Butona basılma sayısı:",
-              style: Theme.of(context).textTheme.labelLarge,
+              style: Theme.of(context)
+                  .textTheme
+                  .labelLarge, //context fonksiyonda parametre olarak aldığımız context
             ),
-            Text(_sayac.toString()),
+            Text(
+              _sayac.toString(),
+              style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                // copywith ile display medium özelliğininin özelliklerini değiştirebiliriz.
+                color: _sayac < 0 ? Colors.red : Colors.green,
+              ),
+            ),
           ],
         ),
       ),
